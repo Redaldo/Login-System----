5dce54fc4fa5f27dda131462762ba659a6ccc998
Ridha Herbache <34724244+Redaldo@users.noreply.github.com>
2020-04-09 18:15:06 +0100

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,6 +1,5 @@
 
 
-gned NOT NULL AUTO_INCREMENT,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `passwort` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `vorname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
